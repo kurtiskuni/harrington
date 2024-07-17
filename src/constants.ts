7fa01b0ea13b3f394014b42c169297ef3c588ac0
Kurtis <kurtiskuni@gmail.com>
2024-07-17 16:39:49 +1000

    first commit

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = 'Tourism Tactix';
+export const SITE_DESCRIPTION = 'Web Agency built;for Adventure Tourism Businesses.'
+export const HOMEPAGE_ARTICLE_LIMIT = 3;
+export const ARTICLES_PER_PAGE = 3;
