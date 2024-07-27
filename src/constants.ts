a3fd662457142abf80386a3a2d9c13da7bc0b682
Kurtis <kurtiskuni@gmail.com>
2024-07-27 16:33:33 +1000

    meta data

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'Tourism Tactix';
-export const SITE_DESCRIPTION = 'Web Agency built;for Adventure Tourism Businesses.'
+export const SITE_TITLE = 'Harrington Electrical & Data Solutions';
+export const SITE_DESCRIPTION = 'Trusted Electrician and Data Solutions Expert in Oakleigh, Serving All of Melbourne. We provided Safe Installations, Repairs, and Upgrades.'
 export const HOMEPAGE_ARTICLE_LIMIT = 3;
 export const ARTICLES_PER_PAGE = 3;
