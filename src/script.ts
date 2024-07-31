396ffc5b901d935999d39949f9f081a48fa9837b
Kurtis <kurtiskuni@gmail.com>
2024-07-31 15:38:58 +1000

    lenis

--- src/script.ts
+++ src/script.ts
@@ -1,16 +1,3 @@
-// Smooth Scroll
-const lenis = new Lenis();
-
-lenis.on('scroll', (e: Event) => {
-  console.log(e);
-});
-
-function raf(time: DOMHighResTimeStamp) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
 
 
 document.addEventListener('astro:after-load', () => {
